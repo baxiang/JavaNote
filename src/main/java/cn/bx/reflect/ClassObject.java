e5024b176fb01be46aaa10ee79fb7ded22480788
baxiang <yangyucug@gmail.com>
2019-09-07 23:37:32 +0800

    update relect

---
+++ src/main/java/cn/bx/reflect/ClassObject.java
@@ -0,0 +1,49 @@
+package cn.bx.reflect;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+class Person{
+    public int id;
+    private String name;
+}
+
+class  Student extends Person{
+    String school;
+    private  String grade;
+    protected String className;
+}
+
+
+public class ClassObject {
+    public static void main(String[] args) {
+        Class<Student> studentClass = Student.class;
+        try {
+            Field name = studentClass.getDeclaredField("grade");
+            System.out.println(name);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            System.out.println("getDeclaredField "+e.getMessage());
+        }
+
+        try {
+            Field name = studentClass.getField("id");
+            System.out.println(name);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            System.out.println("getField "+e.getMessage());
+        }
+
+        Field[] filed1 = studentClass.getDeclaredFields();
+
+        for ( Field f : filed1 ) {
+            System.out.println("Declared Field :"+f.getName());
+        }
+
+        Field[] filed2 = studentClass.getFields();
+
+        for ( Field f : filed2 ) {
+            System.out.println("Field :"+f.getName());
+        }
+    }
+}
