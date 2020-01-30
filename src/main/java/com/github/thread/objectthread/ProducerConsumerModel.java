9997931c18aada776559c7ec68c85b66c63b585b
baxiang <baxiang@roobo.com>
2020-01-30 21:17:37 +0800

    update producer

--- src/main/java/com/github/thread/objectthread/ProducerConsumerModel.java
+++ src/main/java/com/github/thread/objectthread/ProducerConsumerModel.java
@@ -1,65 +1,77 @@
 package com.github.thread.objectthread;
 
-
-import java.util.Date;
 import java.util.LinkedList;
-class EventStorage{
+import java.util.Random;
+
+class EventStorage {
     private Integer maxSize;
-    private LinkedList<Date> storage;
-    public EventStorage(){
-        maxSize=10;
+    private LinkedList<Integer> storage;
+    private Random random;
+
+    public EventStorage() {
+        maxSize = 10;
         storage = new LinkedList<>();
+        random = new Random();
     }
-    public synchronized void put(){
-        while (storage.size()== maxSize){
+
+    public synchronized void put() {
+        while (storage.size() == maxSize) {
             try {
                 wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        storage.add(new Date());
-        System.out.println("当前生产了"+storage.size()+"个产品");
+        Integer p = Integer.valueOf(random.nextInt(100) + 1);
+        storage.add(p);
+        System.out.println("生产:" + p + "\t 总共" + storage.size() + "个");
         notify();
     }
-    public synchronized void take(){
-        while (storage.size()==0){
+
+    public synchronized void take() {
+        while (storage.size() == 0) {
             try {
                 wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        System.out.println("拿到了"+storage.poll()+"\t 现在还剩下"+storage.size());
+        System.out.println("消费:" + storage.poll() + "\t 剩下" + storage.size() + "个");
         notify();
     }
 }
-class Producer implements Runnable{
+
+class Producer implements Runnable {
     private EventStorage storage;
-    public Producer(EventStorage storage){
+
+    public Producer(EventStorage storage) {
         this.storage = storage;
     }
+
     @Override
     public void run() {
-        for (int i=0;i<100;i++){
+        for (int i = 0; i < 100; i++) {
             storage.put();
         }
     }
 }
-class Consumer implements Runnable{
+
+class Consumer implements Runnable {
     private EventStorage storage;
-    public Consumer(EventStorage storage){
+
+    public Consumer(EventStorage storage) {
         this.storage = storage;
     }
+
     @Override
     public void run() {
-        for (int i=0;i<100;i++){
+        for (int i = 0; i < 100; i++) {
             storage.take();
         }
     }
 }
-public class ProducerConsumerModel {
 
+public class ProducerConsumerModel {
     public static void main(String[] args) {
         EventStorage storage = new EventStorage();
         Producer producer = new Producer(storage);
@@ -67,6 +79,4 @@ public class ProducerConsumerModel {
         new Thread(producer).start();
         new Thread(consumer).start();
     }
-
-
 }
